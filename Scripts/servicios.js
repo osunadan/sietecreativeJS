fcddc9998f59a2ae746f8f0c3692b09241c58d5e
osunadan <osuna_dan@outlook.com>
2022-08-16 01:53:43 -0500

    Manipulación del DOM & Eventos

--- Scripts/servicios.js
+++ Scripts/servicios.js
@@ -1,15 +1,13 @@
 // Aquí estoy declarando un class, que sera el constructor de cada uno de los objetos que describen los servicios ofrecidos por la empresa
 class servicio {
-  constructor(nombre, precioEstimado, precio, tiempoEstimado) {
+  constructor(nombre, precioEstimado, tiempoEstimado) {
     this.nombre = nombre;
-    this.precioEstimado = "A partir de " + precioEstimado + " mxn";
-    this.precio = precio;
-    this.tiempoEstimado =
-      "Tiempo estimado de entrega: " + "Minimo " + tiempoEstimado + " Días";
+    this.precioEstimado = precioEstimado;
+    this.tiempoEstimado = tiempoEstimado;
   }
 
   sumaIVA() {
-    return this.precio * 1.16;
+    return this.precioEstimado * 1.16;
   }
 }
 
@@ -17,10 +15,10 @@ class servicio {
 let servicios = [];
 
 // Creo cada uno de los objetos y los agrego al array
-servicios.push(new servicio("fotografia", 2000, 2000, 4));
-servicios.push(new servicio("video", 3500, 3500, 6));
-servicios.push(new servicio("animacion", 6500, 6500, 10));
-servicios.push(new servicio("diseño", 800, 800, 3));
+servicios.push(new servicio("fotografia", 2000, 4));
+servicios.push(new servicio("video", 3500, 6));
+servicios.push(new servicio("animacion", 6500, 10));
+servicios.push(new servicio("diseño", 800, 3));
 
 // Aquí guardo en una variable los resultados de busqueda que el usuario puede solicitar, buscandolos en los objetos que se encuentran en el array
 let fotografia = servicios.find((servicio) => servicio.nombre === "fotografia");
@@ -29,10 +27,113 @@ let animacion = servicios.find((servicio) => servicio.nombre === "animacion");
 let diseño = servicios.find((servicio) => servicio.nombre === "diseño");
 
 // En esta variable guardo el prompt con el que el usuario indicara que tipo de servicio busca
+/*
 let busqueda = prompt(
   "¿Que tipo de servicio necesitas? (Fotografía, Video, Animacion, Diseño)"
 );
+*/
 
+// Aquí guardo en variables el acceso a cada uno de los botones
+let botonVideo = document.querySelector(
+  "#contenedorServicios #contenedorBotones #botonVideo"
+);
+let botonFotografia = document.querySelector(
+  "#contenedorServicios #contenedorBotones #botonFotografia"
+);
+let botonAnimacion = document.querySelector(
+  "#contenedorServicios #contenedorBotones #botonAnimacion"
+);
+let botonDiseño = document.querySelector(
+  "#contenedorServicios #contenedorBotones #botonDiseño"
+);
+
+// Aquí el aceso a los parrafos que mostraran a información
+const parrafoPrecioEstimado = document.querySelector(
+  "#contenedorServicios #contenedorBotones .precioEstimado"
+);
+const parrafoTiempoEstimado = document.querySelector(
+  "#contenedorServicios #contenedorBotones .tiempoEstimado"
+);
+const parrafoPrecioIva = document.querySelector(
+  "#contenedorServicios #contenedorBotones .precioConIva"
+);
+
+// Defino los eventos para cada boton
+botonVideo.addEventListener("click", respuestaVideo);
+botonFotografia.addEventListener("click", respuestaFoto);
+botonAnimacion.addEventListener("click", respuestaAnimacion);
+botonDiseño.addEventListener("click", respuestaDiseño);
+
+// Defino la función para cada evento
+function respuestaVideo() {
+  console.log(video);
+  parrafoPrecioEstimado.innerText =
+    "Precio a partir de " + video.precioEstimado + " mxn";
+  parrafoTiempoEstimado.innerText =
+    "Tiempo estimado de entrega: " +
+    "Minimo " +
+    +video.tiempoEstimado +
+    " días";
+  parrafoPrecioIva.innerText =
+    "El precio inicial con IVA seria " + Math.round(video.sumaIVA());
+  console.log(
+    "El precio inicial con IVA seria " +
+      JSON.stringify(Math.round(video.sumaIVA()))
+  );
+}
+
+function respuestaFoto() {
+  console.log(fotografia);
+  parrafoPrecioEstimado.innerText =
+    "Precio a partir de " + fotografia.precioEstimado + " mxn";
+  parrafoTiempoEstimado.innerText =
+    "Tiempo estimado de entrega: " +
+    "Minimo " +
+    +fotografia.tiempoEstimado +
+    " días";
+  parrafoPrecioIva.innerText =
+    "El precio inicial con IVA seria " + Math.round(fotografia.sumaIVA());
+  console.log(
+    "El precio inicial con IVA seria " +
+      JSON.stringify(Math.round(fotografia.sumaIVA()))
+  );
+}
+
+function respuestaAnimacion() {
+  console.log(animacion);
+  parrafoPrecioEstimado.innerText =
+    "Precio a partir de " + animacion.precioEstimado + " mxn";
+  parrafoTiempoEstimado.innerText =
+    "Tiempo estimado de entrega: " +
+    "Minimo " +
+    +animacion.tiempoEstimado +
+    " días";
+  parrafoPrecioIva.innerText =
+    "El precio inicial con IVA seria " + Math.round(animacion.sumaIVA());
+  console.log(
+    "El precio inicial con IVA seria " +
+      JSON.stringify(Math.round(animacion.sumaIVA()))
+  );
+}
+
+function respuestaDiseño() {
+  console.log(diseño);
+  parrafoPrecioEstimado.innerText =
+    "Precio a partir de " + diseño.precioEstimado + " mxn";
+  parrafoTiempoEstimado.innerText =
+    "Tiempo estimado de entrega: " +
+    "Minimo " +
+    +diseño.tiempoEstimado +
+    " días";
+  parrafoPrecioIva.innerText =
+    "El precio inicial con IVA seria " + Math.round(diseño.sumaIVA());
+  console.log(
+    "El precio inicial con IVA seria " +
+      JSON.stringify(Math.round(diseño.sumaIVA()))
+  );
+}
+
+/*
 // Por ultimo con este switch se ejecuta la busqueda respecto a la entrada del usuario
 switch (busqueda.toLowerCase()) {
   case "fotografia":
@@ -95,3 +196,4 @@ switch (busqueda.toLowerCase()) {
     alert("Ingresa un valor correcto");
     break;
 }
+*/
